3d7d88d1a171c5e2a985466218af8d954fcd92ab
yoochanhong <chanhongy6@naver.com>
2024-03-30 12:54:20 +0900

    feat :: 의존성 주입 locator 추가

---
+++ lib/di/locator.dart
@@ -0,0 +1,28 @@
+import 'package:get_it/get_it.dart';
+import 'package:lotura_desktop/data/laundry/repository/laundry_repository.dart';
+import 'package:lotura_desktop/domain/laundry/repository/laundry_repository.dart';
+import 'package:lotura_desktop/domain/laundry/use_case/get_all_laundry_room_state_use_case.dart';
+import 'package:lotura_desktop/domain/laundry/use_case/get_laundry_room_state_use_case.dart';
+import 'package:lotura_desktop/presentation/laundry_room/bloc/laundry_room_bloc.dart';
+
+final locator = GetIt.instance;
+
+Future<void> initLocator() async {
+  //repository
+  locator.registerSingleton<LaundryRepository>(LaundryRepositoryImpl());
+
+  //use_case
+  locator.registerSingleton<GetAllLaundryRoomStateUseCase>(
+      GetAllLaundryRoomStateUseCase(
+          laundryRepository: locator<LaundryRepository>()));
+
+  locator.registerSingleton<GetLaundryRoomStateUseCase>(
+      GetLaundryRoomStateUseCase(
+          laundryRepository: locator<LaundryRepository>()));
+
+  //bloc
+  locator.registerSingleton(LaundryRoomBloc(
+    getLaundryRoomStateUseCase: locator<GetLaundryRoomStateUseCase>(),
+    getAllLaundryRoomStateUseCase: locator<GetAllLaundryRoomStateUseCase>(),
+  ));
+}
