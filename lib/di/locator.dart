07feaa2cf697889103b2c48037a05622f2cffcf6
yoochanhong <chanhongy6@naver.com>
2024-03-30 15:24:37 +0900

    feat :: datasource 의존성 주입

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:lotura_desktop/data/laundry/data/laundry_data_source.dart';
 import 'package:lotura_desktop/data/laundry/repository/laundry_repository.dart';
 import 'package:lotura_desktop/domain/laundry/repository/laundry_repository.dart';
 import 'package:lotura_desktop/domain/laundry/use_case/get_all_laundry_room_state_use_case.dart';
@@ -8,8 +9,12 @@ import 'package:lotura_desktop/presentation/laundry_room/bloc/laundry_room_bloc.
 final locator = GetIt.instance;
 
 Future<void> initLocator() async {
+  //data_source
+  locator.registerSingleton<LaundryDataSource>(LaundryDataSource());
+
   //repository
-  locator.registerSingleton<LaundryRepository>(LaundryRepositoryImpl());
+  locator.registerSingleton<LaundryRepository>(
+      LaundryRepositoryImpl(dataSource: locator<LaundryDataSource>()));
 
   //use_case
   locator.registerSingleton<GetAllLaundryRoomStateUseCase>(
