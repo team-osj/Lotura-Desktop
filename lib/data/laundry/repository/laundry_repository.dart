7a0de0659d0dcc189d401438282d1e620c13e478
yoochanhong <chanhongy6@naver.com>
2024-03-30 15:26:34 +0900

    refactor :: datasource 연결

--- lib/data/laundry/repository/laundry_repository.dart
+++ lib/data/laundry/repository/laundry_repository.dart
@@ -1,17 +1,15 @@
-import 'dart:convert';
-
-import 'package:lotura_desktop/data/laundry/data/fake_json.dart';
-import 'package:lotura_desktop/data/laundry/dto/response/laundry_response.dart';
+import 'package:lotura_desktop/data/laundry/data/laundry_data_source.dart';
 import 'package:lotura_desktop/domain/laundry/entity/machine_entity.dart';
 import 'package:lotura_desktop/domain/laundry/repository/laundry_repository.dart';
 
 class LaundryRepositoryImpl implements LaundryRepository {
+  final LaundryDataSource dataSource;
+
+  LaundryRepositoryImpl({required this.dataSource});
+
   @override
-  Future<List<MachineEntity>> getAllLaundryList() async {
-    return (jsonDecode(fakeJsonData) as List<dynamic>)
-        .map((i) => LaundryResponse.fromJson(i).toEntity())
-        .toList();
-  }
+  Future<List<MachineEntity>> getAllLaundryList() =>
+      dataSource.getAllLaundryList();
 
   @override
   Stream<MachineEntity> get laundryList => throw UnimplementedError();
